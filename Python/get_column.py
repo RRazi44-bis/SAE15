d05eb81551309380755f8844a7f39f45d1294f6e
RRazi44-bis <azizi.razi.2006@gmail.com>
2025-11-07 14:32:06 +0100

    Correct get_column.py

--- Python/get_column.py
+++ Python/get_column.py
@@ -1,14 +1,16 @@
 # Zidane IKHLEF
 import csv
 
-column = []
 
-with open('experimentations_5G.csv', newline='', encoding='CP1252') as csvfile:
-    reader = csv.reader(csvfile, delimiter=';')  # Essaie aussi ',' si nécessaire
-    for line in reader:
-        column.append(line[2])  # Récupère la 3e colonne
-
+def get_column(indice_x):
+    column = []
+    with open('experimentations_5G.csv', newline='', encoding='CP1252') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')  # Essaie aussi ',' si nécessaire
+        for line in reader:
+            column.append(line[indice_x])  # Récupère la 3e colonne
+    return column
+    
 # Affiche la colonne récupérée
 print("Contenu de la colonne 3 :")
-for value in column:
+for value in get_column(2):
     print(value)
