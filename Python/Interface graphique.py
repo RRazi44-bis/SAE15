c553ff21a8d35603d6b27140ecd6a5a3f859a0f9
nonosaxuniv <hugo.coulon.univ@gmail.com>
2025-11-13 14:19:51 +0100

    Ajout de la dernière version de l'IG
    Modification dans l'interface graphique pour faire apparaitre les graphique en html, des boutons auront pour objectif d'ouvrir ses pages

--- Python/Interface graphique.py
+++ Python/Interface graphique.py
@@ -1,16 +1,19 @@
 import tkinter as tk                    #Hugo
 from tkinter import ttk
 from PIL import Image, ImageTk          #importation des modules nécéssaires
+import os
 
 fenetre=tk.Tk()
 fenetre.title("Interface graphique")
-fenetre.geometry("1280x720")            # création de la fenêtre 
+fenetre.geometry("720x640")            # création de la fenêtre 
 
 frm=ttk.Frame(fenetre)
 frm.grid()
 
 titre=ttk.Label(frm,text="Ensemble des diagrammes")
-titre.grid(column=0,row=0,pady=50)                        #titre 
+titre.grid(column=0,row=0,pady=20)                        #titre 
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 frequence=ttk.Label(frm,text="Diagramme de l'utilisation des fréquences")                   #Création du texte pour les diagrammes
 frequence.grid(column=0,row=1,pady=0,padx=0)
@@ -21,35 +24,29 @@ region.grid(column=0,row=2,pady=0,padx=0)
 experimentateur=ttk.Label(frm,text="Diagramme des expérimentateurs les plus représentés")   #Création du texte pour les diagrammes
 experimentateur.grid(column=0,row=3,pady=0,padx=0)
 
-img=Image.open("chat.jpg")
-img=img.resize((300,200))
-photo=ImageTk.PhotoImage(img)                        #création de l'image 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def actionfrequence():
-    canvas = tk.Canvas(fenetre,width=300,height=200)
-    canvas.grid()
-    canvas.create_image(0, 0, image=photo, anchor="nw")            #fonction qui fera apparaitre le diagramme
+def action_frequence():
+    os.system("start Diagramme_des_fréquences.html")            #fonction qui fera apparaitre le diagramme avec du html
 
-def actionregion():
-    canvas = tk.Canvas(fenetre,width=300,height=200)
-    canvas.grid()
-    canvas.create_image(0, 0, image=photo, anchor="nw")             #fonction qui fera apparaitre le diagramme    
+def action_region():
+    os.system("start Diagramme_des_régions.html")             #fonction qui fera apparaitre le diagramme avec du html
 
-def actionexperimentateur():
-    canvas = tk.Canvas(fenetre,width=300,height=200)
-    canvas.grid()
-    canvas.create_image(0, 0, image=photo, anchor="nw")             #fonction qui fera apparaitre le diagramme
+def action_experimentateur():
+    os.system("start Diagramme_des_expérimentateurs.html")            #fonction qui fera apparaitre le diagramme avec du html
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-boutonfrequence=ttk.Button(frm,text="Ouvrir",command=actionfrequence)
+boutonfrequence=ttk.Button(frm,text="Ouvrir",command=action_frequence)
 boutonfrequence.grid(column=1,row=1)
 
-boutonregion=ttk.Button(frm,text="Ouvrir",command=actionregion)
+boutonregion=ttk.Button(frm,text="Ouvrir",command=action_region)
 boutonregion.grid(column=1,row=2)
 
-boutonexperimentateur=ttk.Button(frm,text="Ouvrir",command=actionexperimentateur)      #Création des bouton pour afficher les diagrammes
+boutonexperimentateur=ttk.Button(frm,text="Ouvrir",command=action_experimentateur)      #Création des bouton pour afficher les diagrammes
 boutonexperimentateur.grid(column=1,row=3)
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 fenetre.mainloop()
